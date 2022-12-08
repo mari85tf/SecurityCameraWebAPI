7164fead5de31e29ee30a6af0bd0304bcc613547
Maria Nijar <r0830560@student.vives.be>
2022-12-08 10:41:42 +0100

    Unit Test Camera Manager

---
+++ SecurityCameraWebAPITests/Managers/CameraManagerTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecurityCameraWebAPI.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityCameraWebAPI.Managers.Tests
+{
+    [TestClass()]
+    public class CameraManagerTests
+    {
+        private CameraManager _cameraManager = new CameraManager();
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            var actual = _cameraManager.GetAll().Count;
+            var expected = 2;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            var actual = _cameraManager.GetById(4).Picture;
+
+            Assert.AreEqual(null, actual);
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            var camera = _cameraManager.GetById(1);
+            _cameraManager.Add(camera);
+
+            var actual = _cameraManager.GetAll().Count;
+            var expected = 3;
+
+            Assert.AreEqual(expected, actual);
+            _cameraManager.Delete(camera.Id);
+
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            var camera = _cameraManager.GetById(1);
+            _cameraManager.Delete(camera.Id);
+
+            var actual = _cameraManager.GetAll().Count;
+            var expected = 1;
+
+            Assert.AreEqual(expected, actual);
+            _cameraManager.Add(camera);
+        }
+    }
+}
