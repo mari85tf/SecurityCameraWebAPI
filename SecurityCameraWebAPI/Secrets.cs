af1a8b00d3cf4dc66aa0727c6ee642542fed5717
Maria Nijar <r0830560@student.vives.be>
2022-12-08 13:31:24 +0100

    Secrets added

---
+++ SecurityCameraWebAPI/Secrets.cs
@@ -0,0 +1,8 @@
+﻿namespace SecurityCameraWebAPI
+{
+    public class Secrets
+    {
+        public static readonly string ConnectionString =
+            "Server=tcp:securitycamera.database.windows.net,1433;Initial Catalog=SecurityCameraDatabase;Persist Security Info=False;User ID=Maria;Password={Kyoto01100115829!};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    }
+}
