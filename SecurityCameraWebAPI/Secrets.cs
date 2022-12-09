5ad7af1299b355add8b744930188b9610a76ebc3
Maria Nijar <r0830560@student.vives.be>
2022-12-09 10:01:58 +0100

    Deleted Secrets file

--- SecurityCameraWebAPI/Secrets.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SecurityCameraWebAPI
-{
-    public class Secrets
-    {
-        public static readonly string ConnectionString =
-            "Server=tcp:securitycamera.database.windows.net,1433;Initial Catalog=SecurityCameraDatabase;Persist Security Info=False;User ID=Maria;Password={Kyoto01100115829!};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-    }
-}
